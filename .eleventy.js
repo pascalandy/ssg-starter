b477bbdf805a0f8bce2575d073c23b7762d7e7e4
Pascal Andy <pascal@firepress.org>
2021-01-03 23:12:12 -0500

    Add eleventyConfig.addPassthroughCopy("img");
    Signed-off-by: Pascal Andy <pascal@firepress.org>

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,17 @@
 const htmlmin = require("html-minifier");
+const Image = require("@11ty/eleventy-img");
 
 module.exports = function (eleventyConfig) {
+
   eleventyConfig.setUseGitIgnore(false);
 
   eleventyConfig.addWatchTarget("./_tmp/style.css");
 
   eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "./style.css" });
+
+  // Output directory: _site
+  // Copy `img/` to `_site/img`
+  eleventyConfig.addPassthroughCopy("img");
 
   eleventyConfig.addPassthroughCopy({
     "./node_modules/alpinejs/dist/alpine.js": "./js/alpine.js",
